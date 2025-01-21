52d8809485a143458458d12550cad24c27814c6f
fahdrahali <rahali.fahd@gmail.com>
2025-01-21 17:35:17 +0100

    create an array tasks

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import "./style.css";
 
+const tasks = [
+  { description: "wash the dishes", completed: true, index: 1 },
+  {
+    description: "complete To Do list project",
+    completed: false,
+    index: 2,
+  },
+];
