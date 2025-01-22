88b2741f035fd91b03d104a4ca8c9dd154670ede
fahdrahali <rahali.fahd@gmail.com>
2025-01-22 10:50:33 +0100

    create a function to display tasks in order of the index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import "./style.css";
 
 const tasks = [
-  { description: "wash the dishes", completed: true, index: 1 },
+  { description: "wash the dishes", completed: true, index: 2 },
   {
     description: "complete To Do list project",
     completed: false,
-    index: 2,
+    index: 4,
   },
+  { description: "read a book", completed: true, index: 3 },
+  { description: "go to the gym", completed: false, index: 1 },
 ];
 
+const sortTasksByIndex = (tasks) => {
+  return  tasks.sort((x, y) => x.index - y.index);
+};
+const displayTasks = () => {
+  const sortedTasked = sortTasksByIndex(tasks);
+  sortedTasked.map((task) => {
+    const li = document.createElement("li");
+    li.textContent = task.description;
+    const list = document.getElementById("task-list");
+    list.append(li);
+  });
+};
 
+document.addEventListener("DOMContentLoaded", () => {
+  displayTasks();
+});
