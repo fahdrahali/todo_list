44df7d3833d1e9ae2f39ff835bbe6b62bbffa4e5
fahdrahali <rahali.fahd@gmail.com>
2025-01-24 11:26:20 +0100

    create a style for todo list

--- src/index.js
+++ src/index.js
@@ -12,15 +12,24 @@ const tasks = [
 ];
 
 const sortTasksByIndex = (tasks) => {
-  return  tasks.sort((x, y) => x.index - y.index);
+  return tasks.sort((x, y) => x.index - y.index);
 };
 const displayTasks = () => {
   const sortedTasked = sortTasksByIndex(tasks);
   sortedTasked.map((task) => {
     const li = document.createElement("li");
-    li.textContent = task.description;
+    const checkBox = document.createElement("input");
+    checkBox.type = "checkbox";
+    checkBox.id = "checkbox";
+    checkBox.name = "checkbox";
+    checkBox.value = task.completed;
+    const label = document.createElement("label");
+    label.for = checkBox.id;
+    label.textContent = task.description;
+    li.append(checkBox);
+    li.append(label);
     const list = document.getElementById("task-list");
-    list.append(li);
+    list.insertBefore(li, document.getElementById("btn"));
   });
 };
 
