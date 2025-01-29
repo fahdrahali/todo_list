7df85ecb7baef0699bba6f454b2e46461e5ad509
fahdrahali <rahali.fahd@gmail.com>
2025-01-29 12:41:47 +0100

    change task item label to input field

--- modules/displayTasks.js
+++ modules/displayTasks.js
@@ -13,9 +13,14 @@ const displayTasks = (tasks) => {
     checkBox.id = "checkbox";
     checkBox.name = "checkbox";
     checkBox.value = task.completed;
-    const label = document.createElement("label");
-    label.for = checkBox.id;
-    label.textContent = task.description;
+
+    // create input task
+    const inputTask = document.createElement("input");
+    inputTask.type = 'text';
+    inputTask.id = 'input-task-item';
+    inputTask.name = 'input-task-item';
+    inputTask.value = task.description;
+    inputTask.setAttribute('disabled', true)
 
     // add a menu dots icon
     const menuDot = new Image();
@@ -23,7 +28,7 @@ const displayTasks = (tasks) => {
     menuDot.id = "menu-dots";
     menuDot.alt = "menu dots";
     li.append(checkBox);
-    li.append(label);
+    li.append(inputTask);
     li.append(menuDot);
     const list = document.getElementById("task-list");
     list.insertBefore(li, document.getElementById("btn"));
